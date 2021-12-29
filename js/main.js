c3a4d236da8cffef1c24fede8136b5127d1f78b8
LINDSEY-KIM <88068327+LINDSEY-KIM@users.noreply.github.com>
2021-12-29 22:35:09 +0900

    flip_exam_ver_2

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function(){
+
+    let deg = 0;
+    setInterval(()=>{
+        deg = deg -90;
+        document.querySelector('.cube').style.transform = 'rotateX(' + deg + 'deg)';
+    }, 1000)
+    
+})();
